baa22d9d31859ec65db6837e4f168baa4a19f0af
illiachumak <71252049+illiachumak@users.noreply.github.com>
2023-05-12 19:59:27 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 
+
 const app = express();
+const server = require('http').Server(app);
+const io = require("socket.io")(server);
+
 app.use(express.json());
 app.use(cors({ origin: true }));
+
+
+
+const rooms = new Map();
+
+app.get('/rooms', function(req, res){
+  rooms.set()
+  res.json(rooms);
+});
 
 app.get('/', (req, res) => {
   res.json({
@@ -14,19 +27,42 @@ app.get('/', (req, res) => {
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
+  const { secret } = req.body;
   
   try{
     const resp = await axios.put(
         "https://api.chatengine.io/users/",
-        {username: username, secret: username, first_name: username},
-        {headers:{"private-key": "6e0a070f-e6f0-4cea-88e9-c2a91828eb12"}}
+        {username: username, secret: secret, first_name: username},
+        {headers:{"private-key": "867d832c-a3fe-428d-9cd7-8941bb86760b"}}
     )
     return res.status(resp.status).json(resp.data)
   } catch(e){
     return res.status(e.response.status).json(e.response.data)
   }
 
-  return res.json({ username: username, secret: "sha256..." });
+  
 });
 
-app.listen(3001);
+app.post("/register", async (req, res) => {
+  const { username } = req.body;
+  const { secret } = req.body;
+  
+  try{
+    const resp = await axios.put(
+        "https://api.chatengine.io/users/",
+        {username: username, secret: secret, first_name: username},
+        {headers:{"private-key": "867d832c-a3fe-428d-9cd7-8941bb86760b"}}
+    )
+    return res.status(resp.status).json(resp.data)
+  } catch(e){
+    return res.status(e.response.status).json(e.response.data)
+  }
+
+  
+});
+
+io.on('connection', socket => {
+
+})
+
+server.listen(3001);
